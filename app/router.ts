371644a2f67d084edd186faf91a0c019cdbd8a44
zaqmjuop <1060007045@qq.com>
2019-10-23 15:41:44 +0800

    注释

--- app/router.ts
+++ app/router.ts
@@ -4,10 +4,10 @@ export default (app: Application) => {
   const { controller, router } = app;
 
   router.get('/', controller.home.index);
-  router.get('/users', controller.user.index);
-  router.get('/books', controller.book.index);
-  router.get('/books/create', controller.book.create);
-  router.get('/users', controller.user.index);
+  // router.get('/users', controller.user.index);
+  // router.get('/books', controller.book.index);
+  // router.get('/books/create', controller.book.create);
+  // router.get('/users', controller.user.index);
   router.post('/users/register', controller.user.register);
   router.post('/users/login', controller.user.login);
   router.post('/users/logout', controller.user.logout);
